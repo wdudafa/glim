a27bc7503e5959f990b0dd6c6022f889ced3b326
Wari <waripamooweidudafa@gmail.com>
2026-02-28 20:46:59 +0000

    taking photos

---
+++ src/app/camera/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+import AuthWrapper from "@/components/AuthWrapper";
+import { useRef, useState } from "react";
+import { Camera } from "react-camera-pro";
+
+export default function CameraPage() {
+  const camera = useRef(null);
+  const [image, setImage] = useState("");
+
+  return (
+    <AuthWrapper>
+      <div>
+        <div>
+          <button
+            style={{ padding: 10 }}
+            onClick={() => {
+              if (camera.current) {
+                const photo = camera.current.takePhoto();
+                setImage(photo);
+              } else {
+                setImage("");
+              }
+            }}
+          >
+            {image ? "Retake Photo" : "Take Photo"}
+          </button>
+          <button
+            style={{ padding: 10 }}
+            onClick={() => {
+              if (camera.current) {
+                camera.current.switchCamera();
+              } else {
+                // TODO Submit image
+              }
+            }}
+          >
+            {image ? "Submit" : "Switch camera"}
+          </button>
+        </div>
+        <div
+          style={{
+            width: "100%",
+            height: "60%",
+            zIndex: -1,
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }}
+        >
+          {!image ? (
+            <Camera
+              ref={camera}
+              errorMessages={{
+                noCameraAccessible: undefined,
+                permissionDenied: undefined,
+                switchCamera: undefined,
+                canvas: undefined,
+              }}
+            />
+          ) : (
+            <img
+              style={{ width: "100%", height: "100%" }}
+              src={image}
+              alt="Image preview"
+            />
+          )}
+        </div>
+      </div>
+    </AuthWrapper>
+  );
+}
