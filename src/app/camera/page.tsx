c80efb80a9d3e5edb94914ec30e08b76c864cd82
Wari <waripamooweidudafa@gmail.com>
2026-03-01 00:57:04 +0000

    Enhance CameraPage with new button styles and icons; integrate Timer component

--- src/app/camera/page.tsx
+++ src/app/camera/page.tsx
@@ -6,6 +6,14 @@ import { Camera } from "react-camera-pro";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import Timer from "@/components/timer";
+import {
+  CiCamera,
+  CiCircleCheck,
+  CiCircleChevLeft,
+  CiRedo,
+  CiTrophy,
+  CiUndo,
+} from "react-icons/ci";
 
 export default function CameraPage() {
   const camera = useRef(null);
@@ -16,13 +24,52 @@ export default function CameraPage() {
   });
   const router = useRouter();
 
+  const buttonStyle: React.CSSProperties = {
+    padding: "10px",
+    borderRadius: "50%",
+    border: "none",
+    backgroundColor: "#ffffff",
+    cursor: "pointer",
+    fontSize: "18px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "80px",
+    height: "80px",
+  };
+
   return (
-    <AuthWrapper >
-      <div className="flex flex-col items-center justify-center h-screen gap-4">
-        <Timer timeLeft={600} camera="true"></Timer>
-        <div>
+    <AuthWrapper>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100vh",
+        }}
+      >
+        <div
+          style={{
+            zIndex: 2,
+            display: "flex",
+            flexDirection: "row",
+            gap: "10px",
+            alignItems: "center",
+            position: "absolute",
+            bottom: "100px",
+          }}
+        >
           <button
-            style={{ padding: 10 }}
+            style={buttonStyle}
+            onClick={() => {
+              router.push("/");
+            }}
+          >
+            <CiCircleChevLeft size={35} />
+          </button>
+          <button
+            style={buttonStyle}
             onClick={() => {
               if (camera.current) {
                 const photo = camera.current.takePhoto();
@@ -32,10 +79,26 @@ export default function CameraPage() {
               }
             }}
           >
-            {image ? "Retake Photo" : "Take Photo"}
+            {image ? <CiRedo size={35} /> : <CiCamera size={35} />}
           </button>
+          <div
+            style={{
+              padding: "10px",
+              borderRadius: 30,
+              border: "none",
+              backgroundColor: "#ffffff",
+              cursor: "pointer",
+              fontSize: "18px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Timer timeLeft={600} camera="true"></Timer>
+          </div>
+
           <button
-            style={{ padding: 10 }}
+            style={buttonStyle}
             onClick={async () => {
               if (camera.current) {
                 camera.current.switchCamera();
@@ -62,26 +125,28 @@ export default function CameraPage() {
               }
             }}
           >
-            {image ? "Submit" : "Switch camera"}
+            {image ? <CiCircleCheck size={35} /> : <CiUndo size={35} />}
           </button>
-
           <button
-            style={{ padding: 10 }}
+            style={buttonStyle}
             onClick={() => {
-              router.push("/");
+              router.push("/leaderboard");
             }}
           >
-            Back
+            <CiTrophy size={35} />
           </button>
         </div>
         <div
           style={{
-            width: "100%",
+            width: "50%",
             height: "60%",
-            zIndex: -1,
+            zIndex: 1,
             position: "absolute",
-            top: 0,
-            left: 0,
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            borderRadius: "30px",
+            overflow: "hidden",
           }}
         >
           {!image ? (
